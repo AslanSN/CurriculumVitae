be56ee7261736e79f2c0910c19ea4066afd727a0
Aslan de Lyss <aslandelyss@gmail.com>
2024-07-02 16:37:26 +0200

    fix: favicon 2

--- components/aslanImage_templ.go
+++ components/aslanImage_templ.go
@@ -25,20 +25,7 @@ func AslanImage() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<img class=\"w-80 h-auto m-4\" src=\"")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		var templ_7745c5c3_Var2 string
-		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(helpers.RepoURL + "/images/AslanSNPhoto.png")
-		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/aslanImage.templ`, Line: 8, Col: 52}
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" alt=\"Placeholder AslanImageAlt\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<img class=\"w-80 h-auto m-4\" src=\"/images/AslanSNPhoto.png\" alt=\"Placeholder AslanImageAlt\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
