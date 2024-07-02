a9ee6a9990f892f1f7c79a2b675d2d75fc3928b8
Aslan de Lyss <aslandelyss@gmail.com>
2024-07-03 00:37:24 +0200

    fix: vercel deployment

--- api/main.go
+++ api/main.go
@@ -1,15 +1,46 @@
 package handler
 
 import (
-    "fmt"
-    "net/http"
+	"net/http"
+
+	"github.com/AslanSN/CurriculumVitae/handlers"
+	"github.com/AslanSN/CurriculumVitae/helpers"
+	handler "github.com/AslanSN/CurriculumVitae/vercel"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello, Vercel!</h1>")
+	// fmt.Fprintf(w, "<h1>Hello, Vercel!</h1>")
+	main()
 }
 
-// func main() {
-//     http.HandleFunc("/", Handler)
-//     http.ListenAndServe(":3000", nil)
-// }
+func main() {
+	e := echo.New()
+
+	// Middlewares
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Vercel connection
+	e.GET("/", echo.WrapHandler(http.HandlerFunc(handler.Handler)))
+
+	// Routes
+	homeHandler := handlers.HomeHandler{}
+
+	data := e.Group("/api/v1/data")
+
+	data.GET(helpers.Api("/info"), homeHandler.HandleShowInfo)
+	data.GET(helpers.Api("/aboutMe"), homeHandler.HandleShowAboutMe)
+	data.GET(helpers.Api("/experience"), homeHandler.HandleShowExperience)
+	data.GET(helpers.Api("/skills"), homeHandler.HandleShowSkills)
+
+	e.Static("/static", "assets")
+	e.Static("/icons", "assets/icons")
+	e.Static("/images", "assets/images")
+	e.Static("/assets/js", "assets/js")
+
+	// Vercel Connection
+	http.Handle("/", e)
+}
