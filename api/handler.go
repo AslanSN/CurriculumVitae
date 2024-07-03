667b6d5a8d62dfdf837794761fdda5d8443a2bda
Aslan de Lyss <aslandelyss@gmail.com>
2024-07-03 14:34:48 +0200

    chore: try new config

--- api/handler.go
+++ api/handler.go
@@ -3,12 +3,12 @@ package handler
 import (
 	"net/http"
 
-	// "github.com/AslanSN/CurriculumVitae/views"
-	// "github.com/a-h/templ"
+	"github.com/AslanSN/CurriculumVitae/views"
+	"github.com/a-h/templ"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// h := templ.Handler(views.Index())
-	// h.ServeHTTP(w, r)
-	main()
+	h := templ.Handler(views.Index())
+	h.ServeHTTP(w, r)
+	// main()
 }
