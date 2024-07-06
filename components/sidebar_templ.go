75576a546482f0a527e823b4cba4e327ad832db6
Aslan de Lyss <aslandelyss@gmail.com>
2024-07-06 16:36:12 +0200

    fix: improve exp cards justification

--- components/sidebar_templ.go
+++ components/sidebar_templ.go
@@ -133,7 +133,7 @@ func Sidebar() templ.Component {
 			var templ_7745c5c3_Var5 string
 			templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(string(helpers.RepoURL + "/icons/whatsapp.svg"))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/sidebar.templ`, Line: 103, Col: 88}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/sidebar.templ`, Line: 101, Col: 88}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 			if templ_7745c5c3_Err != nil {
